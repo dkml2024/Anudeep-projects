fe6f55d406235f45296c6ed46a2fc67b3693d43f
dkml2024 <dkmlakshmi.27@gmail.com>
2024-05-14 22:43:50 +0530

    Add files via upload
    Lab1 files added

---
+++ LAB 1/Testprimitivetypes.java
@@ -0,0 +1,27 @@
+package apjsfa;
+
+public class Testprimitivetypes {
+
+	public static void main(String[] args) {
+		
+        char c = 'H';
+        System.out.println("char  "  +c);
+        int i = 15;
+        System.out.println("int   "   +i);
+        byte b = 8;
+        System.out.println("byte  "  +b );
+        short s = 45;
+        System.out.println("short  "  +s);
+        double d = 6.68;
+        System.out.println("double " +d);
+        float f = 5.483f;
+        System.out.println("float "  +f );
+        long l = 15151;
+        System.out.println("long  "   +l);
+        boolean bool= true;
+        System.out.println("boolean " +bool);
+ 
+        
+	}
+
+}
---
+++ LAB 1/helloworld.java
@@ -0,0 +1,11 @@
+package firstsamplepjt;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
