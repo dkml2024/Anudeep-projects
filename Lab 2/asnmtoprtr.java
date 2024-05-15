380191f50a31efee3ecf35c2a456b2cc8f5ef14c
dkml2024 <dkmlakshmi.27@gmail.com>
2024-05-15 10:05:09 +0530

    Add files via upload
    Lab2 file added

---
+++ Lab 2/asnmtoprtr.java
@@ -0,0 +1,20 @@
+package apjsfa;
+
+public class asnmtoprtr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+        String name;
+ 
+        
+        num = 100;
+        name = "it is the persentage";
+ 
+        
+        System.out.println("number: "   + num);
+        System.out.println("name:  "    + name);
+
+	}
+
+}
