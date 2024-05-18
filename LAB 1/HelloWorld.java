4e8501e1648b83790195354d5bbdf0f40449abf3
dkml2024 <dkmlakshmi.27@gmail.com>
2024-05-18 16:18:44 +0530

    Add files via upload

---
+++ LAB 1/HelloWorld.java
@@ -0,0 +1,15 @@
+package apjfsa;
+
+
+public class HelloWorld {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+		
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
---
+++ LAB 1/Swapandadition.java
@@ -0,0 +1,25 @@
+package apjfsa;
+
+public class Swapandadition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a=10;
+		int b=25;
+		
+		System.out.println("Before Swap");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		
+		int c = a;
+		a = b;
+		b =c;
+		
+		System.out.println("After swap");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+            
+	}
+
+}
