cd7557f217a040e30f454ec47ecfe2e63d9c688a
dkml2024 <dkmlakshmi.27@gmail.com>
2024-05-31 13:36:09 +0530

    Add files via upload

---
+++ Lab 7/Lab 7 - Copy/ASCIIToChar.java
@@ -0,0 +1,16 @@
+package lab7asngmnts;
+
+public class ASCIIToChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		char ch = 'c';
+        int asciiValue = (int) ch;
+        System.out.println("The ASCII value of '" + ch + "' is: " + asciiValue);
+    }
+	}
+
+
