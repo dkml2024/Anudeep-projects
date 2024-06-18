bbea8b40089761687c0e1293f136b71b0c7bfbb2
dkml2024 <dkmlakshmi.27@gmail.com>
2024-06-18 09:09:54 +0530

    Add files via upload

---
+++ Lab 12/MyTest.java
@@ -0,0 +1,26 @@
+package anudeep;
+class Eval {
+
+public static void main(String args[ ]) {
+
+Integer m = Integer.parseInt(args[0]);
+
+Integer n = m;
+
+m--;
+
+m++;
+
+System.out.println(m == n);
+
+}
+
+}
+
+
+
+
+
+	
+	
+
---
+++ Lab 12/TodayTest.java
@@ -0,0 +1,26 @@
+package anudeep;
+
+public class TodayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		try {
+
+			args = null;
+
+			args[0] = "test";
+
+			System.out.println(args[0]);
+
+			} catch (Exception ex) {
+
+			System.out.println("Exception");
+
+			}
+
+			}
+	}
+
+
